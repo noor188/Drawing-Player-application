7f11ea4a644ba03ca2d8753a59e10114566dd3a3
noor188 <noormmlk@gmail.com>
2025-03-17 13:01:37 -0600

    setup project and dependencies

---
+++ src/main/java/com/drawingPlayer/org/DrawingPlayer.java
@@ -0,0 +1,13 @@
+package com.drawingPlayer.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingPlayer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingPlayer.class, args);
+	}
+
+}
---
+++ src/test/java/com/drawingPlayer/org/DrawingPlayerTests.java
@@ -0,0 +1,13 @@
+package com.drawingPlayer.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingPlayerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
