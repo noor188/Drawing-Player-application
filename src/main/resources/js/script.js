8b851f968caed7bb81a5019de1975ddafe167525
noor188 <noormmlk@gmail.com>
2025-04-17 12:25:38 -0600

    feat: add clear button to HTML file with onclick event to reset canvas

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -9,9 +9,17 @@ function drawRectangleAtClick(event) {
     const y = event.clientY - rect.top;
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
-    ctx.fillRect( x - 50, y - 40, 100, 80);
+    ctx.fillRect( x, y, 100, 80);
     console.log("event.clientX", event.clientX);
     console.log("rect.left", rect.left);
+    console.log("event.clientY", event.clientY);
+    console.log("rect.top", rect.top);
 }
 
 canvas.addEventListener('mousedown', drawRectangleAtClick);
+
+
+// Clear canvas once clicked on clear
+function clearCanvas(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+}
