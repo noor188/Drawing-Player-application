1c627d62977fb37f28773004a26c83cb72fb9a0e
noor188 <noormmlk@gmail.com>
2025-04-18 15:08:54 -0600

    fix(canvas): attempt to correct offset issue

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -5,18 +5,25 @@ const ctx = canvas.getContext('2d');
 
 function drawRectangleAtClick(event) {
     const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    console.log("Canvas left top width height", rect);
+    const x = event.clientX - rect.x;
+    const y = event.clientY - rect.y;
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect( x, y, 100, 80);
     console.log("event.clientX", event.clientX);
     console.log("rect.left", rect.left);
+    console.log("left offset",  event.clientX - rect.left);
     console.log("event.clientY", event.clientY);
     console.log("rect.top", rect.top);
+    console.log("top offset", event.clientY - rect.top);
 }
 
+
 canvas.addEventListener('mousedown', drawRectangleAtClick);
+window.addEventListener('resize', () => {
+    rect = canvas.getBoundingClientRect();
+});
 
 
 // Clear canvas once clicked on clear
