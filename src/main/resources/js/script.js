60c24e2bbf62aa36704edd69df816a1da84a1a2f
noor188 <noormmlk@gmail.com>
2025-04-21 12:17:35 -0600

    feat(front-end): add canvas HTML5 element to Home.html, style to style.css, and scripting code to script.js to draw on canvas

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -1,32 +1,10 @@
-// draw a rectangle on canvas
 
+const container = document.querySelector('.container');
 const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
 
-function drawRectangleAtClick(event) {
-    const rect = canvas.getBoundingClientRect();
-    console.log("Canvas left top width height", rect);
-    const x = event.clientX - rect.x;
-    const y = event.clientY - rect.y;
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'blue';
-    ctx.fillRect( x, y, 100, 80);
-    console.log("event.clientX", event.clientX);
-    console.log("rect.left", rect.left);
-    console.log("left offset",  event.clientX - rect.left);
-    console.log("event.clientY", event.clientY);
-    console.log("rect.top", rect.top);
-    console.log("top offset", event.clientY - rect.top);
-}
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+let ctx = canvas.getContext('2d');
 
 
-canvas.addEventListener('mousedown', drawRectangleAtClick);
-window.addEventListener('resize', () => {
-    rect = canvas.getBoundingClientRect();
-});
-
-
-// Clear canvas once clicked on clear
-function clearCanvas(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-}
