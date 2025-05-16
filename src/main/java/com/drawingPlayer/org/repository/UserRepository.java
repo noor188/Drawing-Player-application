1b8a25bbc9d5467bf8103c96b1f3e2c783c2ae29
noor188 <noormmlk@gmail.com>
2025-05-16 11:31:42 -0600

    feat(controller): add /home end point and home.html for front end

--- src/main/java/com/drawingPlayer/org/repository/UserRepository.java
+++ src/main/java/com/drawingPlayer/org/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.drawingPlayer.org.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String username);
 }
